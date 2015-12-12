bd8669befc986cc6514c01860e45223dee0fa026
Matt Solt <mattsolt@gmail.com>
2015-12-12 15:25:46 -0500

    ignore codeclimate.com in vcr config

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,6 +3,7 @@ require 'vcr'
 
 VCR.configure do |c|
   c.ignore_localhost = true
+  c.ignore_hosts 'codeclimate.com'
   c.cassette_library_dir = File.join(File.dirname(__FILE__), '..', 'cassettes')
   c.hook_into :webmock #:typhoeus, :faraday, :fakeweb, or :webmock
   c.default_cassette_options = { :record => :new_episodes }
